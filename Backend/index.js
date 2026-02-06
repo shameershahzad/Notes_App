91bce1e731de2937c77b01a5bcc924c47cbbba72
Shameer Shahzad <shameershazad156@gmail.com>
2026-02-06 17:26:37 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,27 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
 const accountRoutes = require("./routes/accountRoutes");
 const notesRoutes = require("./routes/notesRoutes");
-const connectDB = require("./config/db");
 const cors = require("cors");
-
-connectDB();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Backend working on Vercel ✅");
 });
 
+app.get("/db-check", (req, res) => {
+  res.json({
+    mongoUriLoaded: !!process.env.MONGO_URI
+  });
+});
+
 app.use("/account", accountRoutes);
 app.use("/notes", notesRoutes);
 
-module.exports = app;   
+module.exports = app;  
